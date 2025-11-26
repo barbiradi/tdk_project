dd1d4501f9ce9e4b6dfe4a8ec6ec0c3f5741d04e
A Te Neved <te@email.com>
2025-11-26 17:11:42 +0100

    Update project_tdk.R with latest changes

--- project_tdk.R
+++ project_tdk.R
@@ -52,9 +52,6 @@ my_data_control_covid = my_data_control_covid %>%
   mutate(journal = factor(journal))
 levels(my_data_control_covid$journal)
 
-rm(my_data_control)
-gc()
-ls()
 
 my_data_control_covid_sliced = my_data_control_covid %>% 
   slice_sample(n = nrow(my_data_covid2)) %>%
@@ -138,66 +135,4 @@ t.test(my_data_RUwar2$acceptance_delay,my_data_control_RUwar_sliced$acceptance_d
 t.test(my_data_RUwar2$publication_delay,my_data_control_RUwar_sliced$publication_delay)
 
 
-
-patterns_3 <- "Israeli[- ]?Palestinian conflict|Israeli[- ]?Palestinian war|Hamas|Gaza|Gaza strip|Israeli conflict|Palestinian conflict|IDF|Israeli ground operation|Israeli military operation|October 7 attack|Oct 7 attack|Al-Aqsa Flood|October 7 massacre|Oct 7 massacre"
-
-my_data_IPconflict1 <- my_data %>%
-  filter(
-    str_detect(title,regex(patterns_3,ignore_case = TRUE)) |
-      str_detect(keywords,regex(patterns_3,ignore_case = TRUE))
-  ) %>%
-  mutate(
-    article_date =as.Date(article_date)
-  )
-
-threshold_IPconflict_start <- as.Date("2023-10-07")
-threshold_IPconflict_end <- max(my_data$article_date, na.rm = TRUE)
-
-my_data_IPconflict2 <- my_data %>%
-  filter(
-    str_detect(title,regex(patterns_3,ignore_case = TRUE)) |
-      str_detect(keywords,regex(patterns_3,ignore_case = TRUE))
-  ) %>%
-  mutate(
-    article_date =as.Date(article_date)
-  ) %>%
-  filter(
-    article_date >= threshold_IPconflict_start,
-    article_date <= threshold_IPconflict_end
-  )
-
-my_data_control_IPconflict = my_data %>%
-  filter(
-    article_date >= threshold_IPconflict_start,
-    article_date <= threshold_IPconflict_end
-  ) %>%
-  semi_join(my_data_IPconflict2, by= "journal") %>%
-  filter(!(str_detect(title, regex(patterns_3,ignore_case = TRUE)) |
-             str_detect(keywords, regex(patterns_3, ignore_case = TRUE))
-  ))
-
-my_data_IPconflict2 = my_data_IPconflict2 %>%
-  mutate(journal = factor(journal))
-levels(my_data_IPconflict2$journal)
-
-my_data_control_IPconflict = my_data_control_IPconflict %>%
-  mutate(journal = factor(journal))
-levels(my_data_control_IPconflict$journal)
-
-my_data_control_IPconflict_sliced = my_data_control_IPconflict %>% 
-  slice_sample(n = nrow(my_data_IPconflict2)) %>%
-  arrange(article_date)
-
-my_data_control_IPconflict_sliced %>%
-  ggplot(aes(x = article_date, y = acceptance_delay)) +
-  geom_point() +
-  geom_smooth()
-
-my_data_IPconflict2 %>%
-  ggplot(aes(x = article_date, y = acceptance_delay)) +
-  geom_point() +
-  geom_smooth()
-
-t.test(my_data_IPconflict2$acceptance_delay,my_data_control_IPconflict_sliced$acceptance_delay)
-t.test(my_data_IPconflict2$publication_delay,my_data_control_IPconflict_sliced$publication_delay)
 
